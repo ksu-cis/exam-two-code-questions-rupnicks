6684fea677e5af3b91c955b1fdd5e4b2b907dc0b
Samantha Rupnick <rupnicks@ksu.edu>
2020-04-10 15:11:00 -0500

    Question 1 Completed

--- Data/Cobbler.cs
+++ Data/Cobbler.cs
@@ -1,19 +1,50 @@
-﻿using System;
+﻿/* Author: Nathan Bean
+ * Edited by: Samantha Rupnick
+ * Class: Cobbler.cs
+ * Q1: Implement the INotifyPropertyChanged interface
+ * Purpose: Holds the data for the Cobbler (yummy btw)
+ */
+
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ExamTwoCodeQuestions.Data
 {
-    public class Cobbler : IOrderItem
+    public class Cobbler : IOrderItem, INotifyPropertyChanged
     {
+        /// <summary>
+        /// Event handler for the propertychanged event
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private FruitFilling fruit;
         /// <summary>
         /// The fruit used in the cobbler
         /// </summary>
-        public FruitFilling Fruit { get; set; }
+        public FruitFilling Fruit
+        {
+            get { return fruit; }
+            set
+            {
+                fruit = value;
+                NotifyOfPropertyChanged("Fruit");
+            }
+        }
 
+        private bool withIceCream = true;
         /// <summary>
         /// If the cobbler is served with ice cream
         /// </summary>
-        public bool WithIceCream { get; set; } = true;
+        public bool WithIceCream
+        {
+            get { return withIceCream; }
+            set
+            {
+                withIceCream = value;
+                NotifyOfPropertyChanged("WithIceCream");
+            }
+        }
 
         /// <summary>
         /// Gets the price of the Cobbler
@@ -37,6 +68,16 @@ namespace ExamTwoCodeQuestions.Data
                 if(WithIceCream) { return new List<string>(); }
                 else { return new List<string>() { "Hold Ice Cream" }; }
             }
+        }
+
+        /// <summary>
+        /// Helper method that invokes the property being passed in as well as the specialinstructions property
+        /// </summary>
+        /// <param name="property"></param>
+        protected void NotifyOfPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
         }
     }
 }
